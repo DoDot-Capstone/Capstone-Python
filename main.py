e2ee509720b352bf25bd415fdd172441c3a4d55a
ckh <whrygns123@naver.com>
2023-11-03 18:09:05 +0900

    gpt api  답변 유니코드에서 한글로 변환

--- main.py
+++ main.py
@@ -1,22 +1,19 @@
-import os
 import openai
 
-openai.organization = "org-1XXm4ES0ovolFy2Ck73BlgG3"
 openai.api_key = "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"
-openai.Model.list()
 
-message = input("물어봐 : ")
+def response (message):
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": message},
+        ],
+        temperature=0,
+        max_tokens=256
+    )
 
-response = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": str(message)},
-    ],
-    temperature=0,
-    max_tokens=256
-)
+    return print(response['choices'][0]['message']['content'])
 
-print(response)
-
-# print(chr(response.choices[0].message.content))
+message = input("물어봐: ")
+response(message)
